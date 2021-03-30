2ffbff6d181e010fc44540347af87eb09010aa34
dwiberg4 <dwiberg4@gmail.com>
2021-03-30 14:26:55 -0600

    Updated a few things in the file

--- PID_balance_arduino.ino
+++ PID_balance_arduino.ino
@@ -67,10 +67,10 @@ void loop() {
   
   /*The tiemStep is the time that elapsed since the previous loop. 
    * This is the value that we will use in the formulas as "elapsedTime" 
-   * in seconds. We work in ms so we haveto divide the value by 1000 
+   * in seconds. We work in ms so we have to divide the value by 1000 
    to obtain seconds*/
 
-  /*Reed the values that the accelerometre gives.
+  /*Read the values that the accelerometer gives.
    * We know that the slave adress for this IMU is 0x68 in
    * hexadecimal. For that in the RequestFrom and the 
    * begin functions we have to put this value.*/
@@ -87,7 +87,7 @@ void loop() {
     * and just make then sum of each pair. For that we shift to the left the high values 
     * register (<<) and make an or (|) operation to add the low values.*/
     
-     Acc_rawX=Wire.read()<<8|Wire.read(); //each value needs two registres
+     Acc_rawX=Wire.read()<<8|Wire.read(); //each value needs two registers
      Acc_rawY=Wire.read()<<8|Wire.read();
      Acc_rawZ=Wire.read()<<8|Wire.read();
 
@@ -111,19 +111,19 @@ void loop() {
      Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
  
    /*Now we read the Gyro data in the same way as the Acc data. The adress for the
-    * gyro data starts at 0x43. We can see this adresses if we look at the register map
-    * of the MPU6050. In this case we request just 4 values. W don¡t want the gyro for 
+    * gyro data starts at 0x43. We can see these addresses if we look at the register map
+    * of the MPU6050. In this case we request just 4 values. W don't want the gyro for 
     * the Z axis (YAW).*/
     
    Wire.beginTransmission(0x68);
-   Wire.write(0x43); //Gyro data first adress
+   Wire.write(0x43); //Gyro data first address
    Wire.endTransmission(false);
    Wire.requestFrom(0x68,4,true); //Just 4 registers
    
-   Gyr_rawX=Wire.read()<<8|Wire.read(); //Once again we shif and sum
+   Gyr_rawX=Wire.read()<<8|Wire.read(); //Once again we shift and sum
    Gyr_rawY=Wire.read()<<8|Wire.read();
  
-   /*Now in order to obtain the gyro data in degrees/seconda we have to divide first
+   /*Now in order to obtain the gyro data in degrees/second we have to divide first
    the raw value by 131 because that's the value that the datasheet gives us*/
 
    /*---X---*/
@@ -134,7 +134,7 @@ void loop() {
    /*Now in order to obtain degrees we have to multiply the degree/seconds
    *value by the elapsedTime.*/
    /*Finnaly we can apply the final filter where we add the acceleration
-   *part that afects the angles and ofcourse multiply by 0.98 */
+   *part that afects the angles and of course multiply by 0.98 */
 
    /*---X axis angle---*/
    Total_angle[0] = 0.98 *(Total_angle[0] + Gyro_angle[0]*elapsedTime) + 0.02*Acceleration_angle[0];
@@ -147,7 +147,7 @@ void loop() {
    
   
 /*///////////////////////////P I D///////////////////////////////////*/
-/*Remember that for the balance we will use just one axis. I've choose the x angle
+/*Remember that for the balance we will use just one axis. I've choosen the x angle
 to implement the PID with. That means that the x axis of the IMU has to be paralel to
 the balance*/
 
@@ -173,7 +173,7 @@ if(-3 <error <3)
 
 /*The last part is the derivate. The derivate acts upon the speed of the error.
 As we know the speed is the amount of error that produced in a certain amount of
-time divided by that time. For taht we will use a variable called previous_error.
+time divided by that time. For that we will use a variable called previous_error.
 We substract that value from the actual error and divide all by the elapsed time. 
 Finnaly we multiply the result by the derivate constant*/
 
@@ -182,7 +182,7 @@ pid_d = kd*((error - previous_error)/elapsedTime);
 /*The final PID values is the sum of each of this 3 parts*/
 PID = pid_p + pid_i + pid_d;
 
-/*We know taht the min value of PWM signal is 1000us and the max is 2000. So that
+/*We know that the min value of PWM signal is 1000us and the max is 2000. So that
 tells us that the PID value can/s oscilate more than -1000 and 1000 because when we
 have a value of 2000us the maximum value taht we could sybstract is 1000 and when
 we have a value of 1000us for the PWM sihnal, the maximum value that we could add is 1000
